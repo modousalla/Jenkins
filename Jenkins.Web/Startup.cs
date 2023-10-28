f64062e664673a237aa76b3d6524abad252e6c8d
Mamadou Mbow <mbom0015@uqar.ca>
2023-10-28 18:48:25 -0400

    Ajout du projet web

--- Jenkins.Web/Startup.cs
+++ Jenkins.Web/Startup.cs
@@ -52,30 +52,5 @@ namespace Jenkins.Web
                 endpoints.MapRazorPages();
             });
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            app.UseCookiePolicy();
-
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
-        }
     }
 }
